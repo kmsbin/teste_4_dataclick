cc5f0fa93e5c0d18ef05bdeb28aac885476019ce
kauli-sabino <kaulisabino27@gmail.com>
2021-03-01 07:35:39 -0300

    fixing that files

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,4 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
  // as duas linhas que carregam as variáveis do .env para variáveis de ambiente 
-   
+?>
